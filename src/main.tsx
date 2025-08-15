c9f236ada41cebbf6018fb0189b47a195e8e44df
ahmadsaeeduddin <ahmadsaeeduddin@gmail.com>
2025-08-15 21:42:25 +0500

    jjj

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// main.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/portfolio2.0">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
